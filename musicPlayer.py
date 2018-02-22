6a02be50272c5ddbdf42844696b6cfe5e581a950
Edwin Lee <edwin11k@gmail.com>
2018-02-22 16:02:13 -0500

    Update musicPlayer.py

--- musicPlayer.py
+++ musicPlayer.py
@@ -15,21 +15,51 @@ class MusicPlayer(MusicHandler):
     
     # Plays the music located at the index given & then obtains boolean input for likability
     def musicPlay(self,fileIndex):
+        print()
         print('Music Playing Initiated!')
         MusicHandler.newFactory.musicFiles.printMusicInfo(fileIndex)
-        play(AudioSegment.from_file(self.newFactory.musicFiles.filePath[fileIndex]))
-        
+        song=AudioSegment.from_file(self.newFactory.musicFiles.filePath[fileIndex])
+        play(song[:1000*60])
+        response=''
         while True:
             answer=input('Do you like the music(Yes|No|Abstain)?[Y|N|A]')
-            print()
+            
             if answer=='Y':
-                MusicHandler.posMusic.append(fileIndex)
-                return True
+                MusicHandler.posMusic.append(fileIndex);response='YES'
+                break
             if answer=='N':
-                MusicHandler.negMusic.append(fileIndex)
-                return False
+                MusicHandler.negMusic.append(fileIndex);response='NO'
+                break
             if answer=='A':
-                MusicHandler.absMusic.append(fileIndex)
-                return None
-            print ('Wrong input: Answer must be N,Y,or A')
+                MusicHandler.absMusic.append(fileIndex);response='ABSTAIN'
+                break
+            print ('Wrong Input: Answer Must Be N,Y,or A')
+        
+        MusicHandler.history[MusicHandler.newFactory.musicFiles.fileName[fileIndex]]=response
+        #print(MusicHandler.history)
+        
+
+        
+    def musicStop(self):        
+        while True:
+            cAnswer=input('Do You Want To Listen To The Next Song(Yes|No)?[Y|N]')
+            if cAnswer=='Y':
+                return False
+            if cAnswer=='N':
+                return True
+            print ('Wrong Input: Answer Must Be N or Y')        
+        
+    def displayHistory(self):
+        totalNumber=len(MusicHandler.posMusic)+len(MusicHandler.negMusic)+len(MusicHandler.absMusic)
+        print()
+        print('Display The User Choice History')
+        print()
+        print('Music Play History: (Title: User Choice)')
+        for key,value in MusicHandler.history.items():
+            print ('{} : {}'.format(key,value))
+        print('User Liked {} ,disliked {}, abstained {} out of {} songs'.format(str(len(MusicHandler.posMusic)),str(len(MusicHandler.negMusic)),str(len(MusicHandler.absMusic)),str(totalNumber)))
+        print('Total Favor Percentage:{:.3f} %'.format(len(MusicHandler.posMusic)*100/totalNumber))
+            
+            
+            
         
