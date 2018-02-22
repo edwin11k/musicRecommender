7e69208d7152c6fd6adbfb2c833a755bdd2358c8
Edwin Lee <edwin11k@gmail.com>
2018-02-22 16:01:41 -0500

    Update musicHandler.py

--- musicHandler.py
+++ musicHandler.py
@@ -7,37 +7,41 @@ Created on Fri Feb 16 11:18:49 2018
 
 from musicFactory import *
 
-defaultDir='C:/Users/edwin/Documents/music/musicSimulator/musicFile/Test'
+defaultDir='C:/Users/edwin/Documents/music/musicSimulator/musicFile/Test2'
 genre='Test'
-# Step will determine the resolution of the Audio segment
-window=0.4;step=0.4 
+window=0.4;step=0.4
 
 class MusicHandler(object):
-    newFactory=MusicFactory();newFactory.loadMusic(genre,defaultDir,window,step)
+    newFactory=MusicFactory()  
+    newFactory.loadMusic(genre,defaultDir,window,step);newFactory.saveMusicInfo()
     #Saving index of the musicfile that received either positive or negative result
     # Class variables to be shared by all instances
-    posMusic=[];negMusic=[];absMusic=[]
-    results=[]
+    posMusic=[];negMusic=[];absMusic=[];results=[]
     
+    #Past choices will be saved in history
+    history={}
+      
     # will be overwritten 
+
     ###player----
-    
     def musicPlay(self):
+        pass
+    
+    def displayHistory(self):
         pass
     
     ## selector-----
     def randomSelect(self):
         pass
-    def mostFavSelect(self):
-        pass
-    
+     
     ## Classifier-----
     def binaryClassifier(self):
         pass
     
     def testClassifier(self):
         pass
+        
+    def mostFavSelect(self):
+        pass
     
-    
-
     
