db1c363ba3adffa73f0dac92b1539fa075ace3d7
Edwin Lee <edwin11k@gmail.com>
2018-02-22 16:00:55 -0500

    Update musicFactory.py

--- musicFactory.py
+++ musicFactory.py
@@ -12,9 +12,12 @@ import os
 class MusicFactory(object):
      
     
-    def loadMusic(self,music_genre=None,directory=os.curdir,window1=0.05,step1=0.02):
+    def loadMusic(self,music_genre=None,directory=os.curdir,window1=0.4,step1=0.4):
         self.musicFiles=Music(dirPath=directory,music_genre=music_genre,window=window1,step=step1)
         return self.musicFiles
     
     def printMusicFileInfo(self):
         print(self.musicFiles)
+        
+    def saveMusicInfo(self):
+        self.musicFiles.saveMusicInfo()
