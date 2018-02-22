e0e3798cb71d79d6e9ded7c7e5b51bb7039dbeeb
Edwin Lee <edwin11k@gmail.com>
2018-02-22 16:02:34 -0500

    Update musicSelector.py

--- musicSelector.py
+++ musicSelector.py
@@ -13,6 +13,9 @@ class MusicSelector(MusicHandler):
     
     #Randomly select an index that are not selected before which are contained either posMusic or negMusic
     def randomSelect(self):
+        print()
+        print('At Least One Positive & One Negative Sample Are Required For Modelling')
+        print()
         size=len(self.newFactory.musicFiles.fileData)
         fileIndex=list(range(size))
         for mem in MusicHandler.posMusic:
@@ -21,10 +24,17 @@ class MusicSelector(MusicHandler):
             fileIndex.remove(mem)
         for mem in MusicHandler.absMusic:
             fileIndex.remove(mem)
-        return random.choice(fileIndex)
+ 
+        if fileIndex:
+            return random.choice(fileIndex)
+        else:
+            return -10000
+            
+        
         
     def mostFavSelect(self):
-        maxIndex=-1000000;maxValue=-0.1;
+        maxIndex=-10000;maxValue=-0.1;
+        print()
         print('Selecting Your Favorite Music!')
         #print(MusicHandler.results)
         #print(MusicHandler.posMusic)
