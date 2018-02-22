62ef34e41a62b777f1037ed86d8a6db38a9431e4
Edwin Lee <edwin11k@gmail.com>
2018-02-22 16:03:26 -0500

    Update util.py

--- util.py
+++ util.py
@@ -6,6 +6,7 @@ Created on Tue Feb 13 19:15:19 2018
 """
 from musicHandler import *
 import numpy as np
+import logging
 
 
     
@@ -17,7 +18,7 @@ def loadMusicFeatures(listMusic,listDir,window,windowStep):
         if window[i]==None and windowStep[i]==None:
             Music=newFactory.loadMusic(mem,listDir[i])
         else:
-            print('Warning : New Step and Window size assigned!')
+            logging.warning('New Step And Window Size Assigned!')
             Music=newFactory.loadMusic(mem,listDir[i],window[i],windowStep[i])
         musicFeatures.append(Music.stFeatures)   
     print('Music Features are loaded')
@@ -31,7 +32,6 @@ def displayMusicInfo(musicType,listDir,window,windowStep):
         #newFactory.printMusicFileInfo()
     
     
-
 
 # Stack the features from various sources in the genre, & Transpose is needed to use list of features
 def featureStack(features):
