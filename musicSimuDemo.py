a4d30c7ea0ce17098d09f22bd7dcb47f2767a296
Edwin Lee <edwin11k@gmail.com>
2018-08-26 12:51:25 -0400

    Update musicSimuDemo.py

--- musicSimuDemo.py
+++ musicSimuDemo.py
@@ -15,8 +15,11 @@ modelDir2='C:/Users/edwin/Documents/music/musicSimulator/musicFile/Test2'
 
 # Path to music data files need to be set in musicHandler.py 
 alpha=0.5;
+coXiter=10;
 
 
+
+    
 
 
 def modelMaker():
@@ -54,20 +57,7 @@ def modelPredictionTester():
     
     selector=MusicSelector();player=MusicPlayer();classifier=MusicClassifier()
     numOfTest=39;currentNum=0;
-#    while True:
-#        print()
-#        print('Finding Random Choice of Music!')
-#        newValue=selector.randomSelect()
-#        if newValue==-10000:
-#            print()
-#            print('Music Data Has Run Out!')
-#            player.displayHistory()
-#            return
-#        
-#        if currentNum>=numOfTest:
-#            break
-#        currentNum+=1;print('Current Music Count:',str(currentNum))
-#        player.musicPlayTwinSimuAnswer(modelDir1,modelDir2,newValue)
+
            
     for newValue in range(numOfTest):
         currentNum+=1;print('Current Music Count:',str(currentNum))
@@ -120,27 +110,72 @@ def musicRecommender():
     
     
     ## feature selection needs to be set in util function.Y
-def musicXorrRecommender():
+def refSVMtest():
     print()
-    print("Music Simulator: Based On Response, It Will Find Your Favorites")
+    print("SVM Training & Test")
     print()
     
     selector=MusicSelector();player=MusicPlayer();classifier=MusicClassifier()
+    totalNumber=selector.musicCount
+    print(totalNumber)
     
-    print("Random Choice of Music")
-    newRandomValue=selector.randomSelect()
-    player.musicPlay(newRandomValue)
-    while True:
-        classifier.computeMeanFeatureXorrSum()
-        newMusicValue=selector.minXorrSelect()
-        if newMusicValue==-1:
-            print('Music has run out!')
-            break
-        else:
-            player.musicPlay(newMusicValue)
+    trainIndex=[4,6,7,11,12,17,18,21,23,34,41,28,29,30,35,36,37,40,38,39]
+    testIndex=[0,1,2,3,5,8,9,10,13,14,15,16,19,20,22,24,25,27,31,32,33]
+    print(len(trainIndex),len(testIndex))
+    selector.addMusicIndex(trainIndex)
+    print(MusicHandler.posMusic,MusicHandler.negMusic)
+    classifier.binaryClassifier()
+    classifier.saveClassifier()
+    classifier.validateClassifier(testIndex)
+    
+    
             
-        
-        
+
+
+def musicCowork():
+    print()
+    print("Random Musics are chosen for label")
+    print()
+    
+    posSample=0;negSample=0;iterI=0;
+    selector=MusicSelector();player=MusicPlayer();classifier=MusicClassifier()
+    # Later label may be assigned in different form
+    while True:
+        print()
+        print('Finding Random Choice of Music!')
+        newValue=selector.randomSelect()
+        if newValue==-10000:
+            print()
+            print('Music Data Has Run Out!')
+            player.displayHistory()
+            return
+        player.musicPlay(newValue)
+        if len(selector.posMusic)>posSample and len(selector.negMusic)>negSample:           
+            break
+    while iterI<coXiter:
+        classifier.binaryCoMLClassifier(mode='MFCC',MLAlgorithm='SVM_Linear')
+        classifier.binaryCoMLClassifier(mode='Chromatic',MLAlgorithm='SVM_Linear')
+        # Find data that are the most outside located from decision boundary
+        minMFCCIndex,maxMFCCIndex=selector.findMostOutData(ML='SVMLinear',mode='MFCC')
+        MusicSelector.posMusic.append(maxMFCCIndex);MusicSelector.negMusic.append(minMFCCIndex)
+        print('Pos',MusicSelector.posMusic);print('Neg',MusicSelector.negMusic)
+        classifier.binaryCoMLClassifier(mode='Chromatic',MLAlgorithm='SVM_Linear')
+        minChromaIndex,maxChromaIndex=selector.findMostOutData(ML='SVMLinear',mode='Chromatic')
+        MusicSelector.posMusic.append(maxChromaIndex);MusicSelector.negMusic.append(minChromaIndex)
+        print('Pos',MusicSelector.posMusic);print('Neg',MusicSelector.negMusic)
+        iterI+=1
+    
+    
+    classifier.saveClassifier("coSVM_MFCC","MFCC")
+    classifier.saveClassifier("coSVM_Chromatic","Chromatic")
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -275,10 +310,10 @@ def musicSimulatorAlpha(modelDir):
     classifier.saveClassifier()
     
         
-    
-    
+refSVMtest()
+#musicCowork()
 #modelMaker()   
-musicXorrRecommender()
+#musicXorrRecommender()
 #musicRecommender()
 #musicSimulator(modelDir1)
 #musicRecommenderAlpha()
